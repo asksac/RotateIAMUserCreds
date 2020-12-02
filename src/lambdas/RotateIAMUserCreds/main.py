c98b9e61dc1ac4a27c8e907c9649b83b5edbc5bb
Hamirwasia <ssacha@a483e7302e4c.ant.amazon.com>
2020-12-01 18:10:46 -0600

    Refactored to use payload param for days_filter

--- src/lambdas/RotateIAMUserCreds/main.py
+++ src/lambdas/RotateIAMUserCreds/main.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import jsonpickle
 import boto3
 from botocore.exceptions import ClientError
 import datetime
@@ -8,9 +7,6 @@ import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-#client = boto3.client('lambda')
-#client.get_account_settings()
 
 # Create IAM client
 iam_client = boto3.client('iam')
@@ -24,7 +20,7 @@ def list_access_key(user, days_filter, status_filter):
   access_keys = iam_client.list_access_keys(UserName = user)
   user_keys = []
   
-  # Some user may have 2 access keys 
+  # user may have 2 access keys 
   for keys in access_keys['AccessKeyMetadata']: 
     key_details = {}
     days = time_diff(keys['CreateDate'])
@@ -61,8 +57,7 @@ def delete_key(user, access_key):
       
 
 def lambda_handler(event, context):
-  # details = iam_client.list_users(MaxItems=300)
-  # print(details)
+  # to limit exposure via this Lambda, we want to restrict IAM user via an environment variable
   user = os.environ.get('IAM_USER_NAME')
   if (not user): 
     return {
@@ -70,16 +65,17 @@ def lambda_handler(event, context):
       'body': 'A valid username not specified via IAM_USER_NAME environment variable'
     }
 
-  age_filter = os.environ.get('IAM_ACCESS_KEY_MIN_AGE')
-  if (not age_filter or not age_filter.isnumeric()): 
+  #days_filter = os.environ.get('IAM_ACCESS_KEY_MIN_AGE')
+  days_filter = event.get('AccessKeyMinAgeInDays')
+  if (not days_filter or not days_filter.isnumeric()): 
     return {
       'statusCode': 500,
-      'body': 'A valid age filter not specified via IAM_ACCESS_KEY_MIN_AGE environment variable'
+      'body': 'A valid value of AccessKeyMinAgeInDays not specified in function payload, e.g. {"AccessKeyMinAgeInDays": "30"}'
     }
 
-  logger.info('Requested IAM_USER_NAME and IAM_ACCESS_KEY_MIN_AGE: {}, {}'.format(user, age_filter))
+  logger.info('Requested IAM_USER_NAME and AccessKeyMinAgeInDays: {}, {}'.format(user, days_filter))
 
-  user_keys = list_access_key(user = user, days_filter = int(age_filter), status_filter = 'Active')
+  user_keys = list_access_key(user = user, days_filter = int(days_filter), status_filter = 'Active')
   new_keys = []
   for access_key in user_keys:
       logger.info('Rotating access key id {} for user {}'.format(access_key['AccessKeyId'], access_key['UserName']))
